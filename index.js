b34a5a0138ac04c11926591508e163b8b003fe0c
Lawson Hung <lhung1@babson.edu>
2019-04-24 22:39:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+   var lis = document.querySelectorAll('ranked-list');
+   
+   for (let i = 0; i < lis.length; i++) {
+     lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+   }
+}
